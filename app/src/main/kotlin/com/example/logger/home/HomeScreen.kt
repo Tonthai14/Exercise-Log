fd787877e218f19540e785d2e642e5d69524abfa
tony <Tonthai14@gmail.com>
2025-06-06 10:53:56 -0500

    All converted kotlin files

--- app/src/main/kotlin/com/example/logger/home/HomeScreen.kt
+++ app/src/main/kotlin/com/example/logger/home/HomeScreen.kt
@@ -3,7 +3,6 @@ package com.example.logger.home
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,12 +18,8 @@ import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -33,17 +28,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.logger.font.cascadiaMonoFamily
-import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
-import java.util.Locale
 
 @Composable
 fun HomeScreen(onNavigateToDay: (date: String) -> Unit) {
@@ -57,66 +47,65 @@ fun HomeScreen(onNavigateToDay: (date: String) -> Unit) {
             .padding(10.dp)
             .fillMaxSize()
     ) {
-        QuickAccessSection(onNavigateToDay, dateFormat)
-        DatePickerSection(onNavigateToDay, dateFormat)
+        val modifier = Modifier
+            .fillMaxWidth(0.7F)
+            .background(
+                color = Color.hsv(269F, 0.05F, 0.35F),
+                shape = RoundedCornerShape(12.dp)
+            )
+            .padding(20.dp)
+        QuickAccessSection(onNavigateToDay, dateFormat, modifier = modifier)
+        DatePickerSection(onNavigateToDay, dateFormat, modifier = modifier)
     }
-
-//    Scaffold(
-//        topBar = { HomePageTopBar(onNavigateToDay) }
-//    ) {
-//        Column(
-//            verticalArrangement = Arrangement.Center,
-//            horizontalAlignment = Alignment.CenterHorizontally,
-//            modifier = Modifier
-//                .padding(it)
-//                .fillMaxSize()
-//        ) {
-//            DayButton(text = "Today's Exercises", onNavigateToDay, sdf.format(today))
-//        }
-//    }
 }
 
 @Composable
-fun QuickAccessSection(onNavigateToDay: (date: String) -> Unit, dateFormat: DateTimeFormatter) {
+fun QuickAccessSection(onNavigateToDay: (date: String) -> Unit,
+                       dateFormat: DateTimeFormatter,
+                       modifier: Modifier = Modifier
+) {
     val today = LocalDate.now()
     val yesterday = today.minusDays(1)
     val tomorrow = today.plusDays(1)
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .background(
-                color = Color.hsv(269F, 0.05F, 0.35F),
-                shape = RoundedCornerShape(12.dp)
-            )
-            .padding(20.dp)
+        modifier = modifier
     ) {
         Text(text = "Quick Access", fontFamily = cascadiaMonoFamily, color = Color.White)
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { onNavigateToDay(today.format(dateFormat)) }) {
+        val buttonModifier = Modifier.fillMaxWidth(0.8F)
+        Button(
+            onClick = { onNavigateToDay(today.format(dateFormat)) },
+            modifier = buttonModifier
+        ) {
             Text(text = "Today")
         }
-        Button(onClick = { onNavigateToDay(yesterday.format(dateFormat)) }) {
+        Button(
+            onClick = { onNavigateToDay(yesterday.format(dateFormat)) },
+            modifier = buttonModifier
+        ) {
             Text(text = "Yesterday")
         }
-        Button(onClick = { onNavigateToDay(tomorrow.format(dateFormat)) }) {
+        Button(
+            onClick = { onNavigateToDay(tomorrow.format(dateFormat)) },
+            modifier = buttonModifier
+        ) {
             Text(text = "Tomorrow")
         }
     }
 }
 
 @Composable
-fun DatePickerSection(onNavigateToDay: (date: String) -> Unit, dateFormat: DateTimeFormatter) {
+fun DatePickerSection(onNavigateToDay: (date: String) -> Unit,
+                      dateFormat: DateTimeFormatter,
+                      modifier: Modifier = Modifier
+) {
     var showDatePicker by remember {mutableStateOf(false)}
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .background(
-                color = Color.hsv(269F, 0.05F, 0.35F),
-                shape = RoundedCornerShape(12.dp)
-            )
-            .padding(20.dp)
+        modifier = modifier
     ) {
         Text("Specific Date", fontFamily = cascadiaMonoFamily, color = Color.White)
         Spacer(modifier = Modifier.height(16.dp))
@@ -134,41 +123,9 @@ fun DatePickerSection(onNavigateToDay: (date: String) -> Unit, dateFormat: DateT
         DatePickerModal(
             onDateSelected = {
                 onNavigateToDay(it)
-                println(it)
             },
-            onDismiss = {showDatePicker = false}
-        )
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun HomePageTopBar(onNavigateToDay: (date: String) -> Unit) {
-    var showDatePicker by remember {mutableStateOf(false)}
-    var selectedDate by remember { mutableStateOf<Long?>(null) }
-
-    TopAppBar(
-        title = {
-            Text(text = "Home", fontWeight = FontWeight.Bold)
-        },
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer,
-            titleContentColor = MaterialTheme.colorScheme.primary
-        ),
-        actions = {
-            IconButton(onClick = {showDatePicker = !showDatePicker}) {
-                Icon(imageVector = Icons.Default.DateRange, contentDescription = "Select Date")
-            }
-        }
-    )
-
-    if (showDatePicker) {
-        DatePickerModal(
-            onDateSelected = {
-                onNavigateToDay(it)
-                println(it)
-            },
-            onDismiss = {showDatePicker = false}
+            onDismiss = {showDatePicker = false},
+            dateFormat = dateFormat
         )
     }
 }
@@ -177,9 +134,9 @@ fun HomePageTopBar(onNavigateToDay: (date: String) -> Unit) {
 @Composable
 fun DatePickerModal(
     onDateSelected: (String) -> Unit,
-    onDismiss: () -> Unit
+    onDismiss: () -> Unit,
+    dateFormat: DateTimeFormatter
 ) {
-    val sdf = SimpleDateFormat("MM-dd-yyyy", Locale.getDefault())
     val datePickerState = rememberDatePickerState()
 
     DatePickerDialog(
@@ -187,8 +144,9 @@ fun DatePickerModal(
         confirmButton = {
             TextButton(
                 onClick = {
-                    println(datePickerState.selectedDateMillis)
-                    onDateSelected(sdf.format(datePickerState.selectedDateMillis))
+                    val selectedDate = datePickerState.selectedDateMillis
+                    val formattedDate = LocalDate.parse(selectedDate.toString(), dateFormat)
+                    onDateSelected(formattedDate.toString())
                     onDismiss()
                 }
             ) {
@@ -200,27 +158,6 @@ fun DatePickerModal(
         }
     ) {
         DatePicker(state = datePickerState)
-    }
-}
-
-@Composable
-fun DayButton(
-    text: String,
-    onNavigateToDay: (date: String) -> Unit,
-    date: String,
-    modifier: Modifier = Modifier
-) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(vertical = 12.dp, horizontal = 24.dp)
-    ) {
-        Button(
-            onClick = { onNavigateToDay(date) },
-            modifier = modifier.fillMaxWidth()
-        ) {
-            Text(text = text)
-        }
     }
 }
 
